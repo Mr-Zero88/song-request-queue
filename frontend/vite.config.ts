06d155ed3df35da885bb7bc147aa51987c614a21
Michael Tews <michael@tews.dev>
2026-04-22 16:41:02 +0200

    chore(frontend): add stylex

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import stylex from "@stylexjs/unplugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+	plugins: [
+		stylex.vite({
+			// StyleX configuration options
+			useCSSLayers: true,
+		}),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+});
